762780862b3195de14f36cd6a817bc28236e7fbe
AndriusBendoraitis1985 <andrius.bendoraitis1985@gmail.com>
2022-12-07 23:00:15 +0200

    initial commit

---
+++ src/test/java/lt/bank/account/demoProject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.bank.account.demoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
