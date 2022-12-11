11079682cffd6d8a44b1f095562bc16707500ac1
AndriusBendoraitis1985 <andrius.bendoraitis1985@gmail.com>
2022-12-11 21:38:02 +0200

    updated schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ opr_id BIGINT NOT NULL AUTO_INCREMENT,
 opr_account_number VARCHAR(128) NOT NULL,
 opr_date TIMESTAMP WITH TIME ZONE NOT NULL,
 opr_beneficiary VARCHAR(128) NOT NULL,
-opr_comment VARCHAR(255) NOT NULL,
+opr_comment VARCHAR(255),
 opr_amount DOUBLE NOT NULL,
 opr_currency VARCHAR(5) NOT NULL,
 PRIMARY KEY (opr_id));
